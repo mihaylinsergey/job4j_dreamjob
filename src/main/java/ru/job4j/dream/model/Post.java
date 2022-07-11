a4b4390c90c7aa852d4ea47dfec50ddb0fdc4214
mihaylinsergey <semi999@mail.ru>
2022-07-11 09:49:47 +0300

    1. Формы. Поля ввода. [#504853]

--- src/main/java/ru/job4j/dream/model/Post.java
+++ src/main/java/ru/job4j/dream/model/Post.java
@@ -8,6 +8,7 @@ public class Post {
     private String name;
     private  String description;
     private LocalDate created;
+    private boolean visible;
 
     public Post(int id, String name) {
         this.id = id;
@@ -51,6 +52,14 @@ public class Post {
 
     public void setCreated(LocalDate created) {
         this.created = created;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
     }
 
     @Override
