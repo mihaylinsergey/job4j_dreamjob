3136691ffd46adfedc0d9addd53e403fc69628a7
mihaylinsergey <semi999@mail.ru>
2022-08-17 08:54:15 +0300

    3. Тестирование базы данных. Liquibase H2 [#504862 #332977]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,15 +1,18 @@
-CREATE TABLE if not exists post (
-                                    post_id SERIAL PRIMARY KEY,
-                                    post_name TEXT,
-                                    post_description TEXT,
-                                    post_visible bool,
-                                    post_city_id int
+CREATE TABLE IF NOT EXISTS post (
+   id SERIAL PRIMARY KEY,
+   name TEXT,
+   description TEXT,
+   created DATE,
+   visible boolean,
+   city_id int
 );
-CREATE TABLE IF NOT EXISTS candidate (
-                                         can_id SERIAL PRIMARY KEY,
-                                         can_name TEXT,
-                                         can_description TEXT,
-                                         can_visible bool,
-                                         can_city_id int,
-                                         can_photo bytea
-)
+
+CREATE TABLE IF NOT EXISTS candidates (
+   id SERIAL PRIMARY KEY,
+   name TEXT,
+   description TEXT,
+   created DATE,
+   visible boolean,
+   city_id int,
+   photo BYTEA
+);
