87d48f4c8ea3a21b654f157e9da2bd479b8a2a61
mihaylinsergey <semi999@mail.ru>
2022-08-23 16:14:22 +0300

    2. HttpSession [#504864]

--- src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
@@ -1,13 +1,23 @@
 package ru.job4j.dreamjob.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.model.User;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class IndexControl {
 
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            user = new User();
+            user.setEmail("Гость");
+        }
+        model.addAttribute("user", user);
         return "index";
     }
 }
